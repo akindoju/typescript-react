d60862118aac00f81da13fe61ca27069f8c47698
akindoju <davidd.akindoju@gmail.com>
2022-04-05 14:30:19 +0100

    Added react-icons and styled card

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "./InputField.css";
 
 interface Props {
@@ -7,9 +8,18 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form
+      className="input"
+      onSubmit={(event) => {
+        handleAdd(event);
+        inputRef.current?.blur();
+      }}
+    >
       <input
+        ref={inputRef}
         type="input"
         placeholder="Enter a task"
         className="input__box"
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,27 @@
+import { Todo } from "../../model";
+import SingleTodo from "../SingleTodo/SingleTodo";
+import "./TodoList.css";
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+  return (
+    <div className="todos">
+      {todos.map((todo) => {
+        return (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
